99122148850f4b6bc2951b816cdb7f48ca4c7616
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-22 03:45:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
   def get_page()
+  
+  end 
+  
+  def get_courses
+    
+  end 
+  
+  def make_courses
   
   end 
   
